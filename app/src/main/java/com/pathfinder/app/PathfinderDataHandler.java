ea0a3eccfdb331100460324110a6ec08bf19cbe7
sgtjond <jpd98@nau.edu>
2019-11-24 20:19:33 -0700

    Changes Made: -Visual Changes  -Side bar and map now show up correctly  -App theme changed to match the NAU colors  -Some elements on the sidebar changed -Implemented all UML Classes, including  -PathFinderDataHandler (Main class in old UML)  -PathfinderMap (MasterMap class in old UML)  -MapEntity (Entity class in old UML)  -NavPoint (Navpoint class in old UML)  -Room (Room class in old UML)  -Building (Building class in old UML)  -BuildingFloor (InteriorMap class in old UML) -Renamed package hierarchy to 'com.pathfinder.app' from 'com.example.pathfinder'

--- app/src/main/java/com/pathfinder/app/PathfinderDataHandler.java
+++ app/src/main/java/com/pathfinder/app/PathfinderDataHandler.java
@@ -2,5 +2,34 @@ package com.pathfinder.app;
 
 class PathfinderDataHandler
 {
+    private PathfinderMap bMap;
+    private PathfinderMap bpMap;
+    private PathfinderMap bsMap;
+
+    private boolean displayBuildings;
+    private boolean displayBluePhones;
+    private boolean displayBusStops;
+
+    public PathfinderDataHandler()
+    {
+        displayBuildings = true;
+        displayBluePhones = false;
+        displayBusStops = false;
+    }
+
+    public void setDisplayBuildings()
+    {
+
+    }
+
+    public void setDisplayBluePhones()
+    {
+
+    }
+
+    public void setDisplayBusStops()
+    {
+
+    }
 
 }
